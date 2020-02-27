76b221448aeebb6d9abd2931db260a68d4364503
7777ty <568318359@qq.com>
2020-02-27 14:50:08 +0800

    deploy

---
+++ precache-manifest.829f2daaa4671e48c843428fa95dbd22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2f38a91d9d374749a",
+    "url": "/booking-1-website/css/app.cf1f290f.css"
+  },
+  {
+    "revision": "0a96e92abb905f741158f500c1c5e1f7",
+    "url": "/booking-1-website/index.html"
+  },
+  {
+    "revision": "1ff2f38a91d9d374749a",
+    "url": "/booking-1-website/js/app.3eb33289.js"
+  },
+  {
+    "revision": "d76fc4b36f5baef414dc",
+    "url": "/booking-1-website/js/chunk-vendors.edab2010.js"
+  },
+  {
+    "revision": "879a6319db462d96496a20fb5c9a7b16",
+    "url": "/booking-1-website/manifest.json"
+  },
+  {
+    "revision": "bb3f5337f1a61896648f4d5be923a324",
+    "url": "/booking-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/booking-1-website/robots.txt"
+  }
+]);
